291f3c4c3e8fb7849134fcbebacf649027dc0e19
Eduardo Yrigoyen <eyrigoyen78@gmail.com>
2022-09-02 12:21:55 -0500

    correcciones de componente empleados

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: "app-empleados",
     templateUrl: "./empleados.component.html",
-    styleUrls: ["./empleados.component.css"]
+    styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent{
 
